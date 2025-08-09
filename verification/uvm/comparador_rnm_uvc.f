728c4554457f86731aebc95c164c6d882167e446
Nami-L <namigtle066@gmail.com>
2025-08-08 18:34:36 -0600

    Eliminar directorio

--- verification/uvm/comparador_rnm_uvc.f
+++
@@ -1,5 +0,0 @@
-+incdir+$GIT_ROOT/verification/uvm/sv
-+incdir+$GIT_ROOT/verification/uvm/sv/seqlib
-
-$GIT_ROOT/verification/uvm/sv/comparador_rnm_uvc_pkg.sv
-$GIT_ROOT/verification/uvm/sv/comparador_rnm_uvc_if.sv
--- verification/uvm/sve.f
+++
@@ -1,8 +0,0 @@
-
-
-+incdir+$GIT_ROOT/verification/uvm/env
-+incdir+$GIT_ROOT/verification/uvm/tests
-+incdir+$GIT_ROOT/verification/uvm/tb
-$GIT_ROOT/verification/uvm/env/top_env_pkg.sv
-$GIT_ROOT/verification/uvm/tests/top_test_pkg.sv
-$GIT_ROOT/verification/uvm/tb/tb.sv
