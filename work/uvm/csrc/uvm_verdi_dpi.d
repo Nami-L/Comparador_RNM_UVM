daa20c7ceb1c70113667d214819105d2651ae933
Nami <namigtle066@gmail.com>
2025-09-03 13:49:49 -0600

    feat: Simulacion sin carpeta work

--- work/uvm/csrc/uvm_verdi_dpi.d
+++
@@ -1,8 +0,0 @@
-uvm_verdi_dpi.o: \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/etc/uvm-1.2/verdi/dpi/uvm_verdi_dpi.cpp \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/include/vpi_user.h \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/include/veriuser.h \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/include/sv_vpi_user.h \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/include/vpi_user.h \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/include/vcs_vpi_user.h \
- /home/EDA/CDS/SYNOPSYS/vcs/U-2023.03-SP2-9/include/sv_vpi_user.h
